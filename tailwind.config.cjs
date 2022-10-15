4c5069c2f17018a415cb3015dc61ce8fcfaea989
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-10-15 23:50:28 +0000

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'body': '#0F172A',
+      'boxes': '#1E293B',
+      'logobg': '#666666',
+      'logofont': '#FFFFFF',
+    }
+    
+  },
+  plugins: [],
+}
