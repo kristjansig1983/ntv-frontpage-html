617ebea692e0760cdca1be0e471248730d6895e6
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-10-20 20:08:57 +0000

    Needs some work
    Needs alot of work on forritun.html and some adjustments on index.html.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
     colors: {
       'body': '#0F172A',
       'boxes': '#1E293B',
       'logobg': '#666666',
       'logofont': '#FFFFFF',
-      'button': '#38BDF8'
+      'button': '#38BDF8',
+      
     },
+  },
   },
   plugins: [],
 }
